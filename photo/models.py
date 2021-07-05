ef3168e860252e2dadd48f647659ce80cec56335
Gaddafi Adamu <gaddafiadamu@Gaddafis-MacBook-Pro.local>
2021-07-05 21:03:10 +0100

    another changes

--- photo/models.py
+++ photo/models.py
@@ -9,7 +9,7 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     # image = CloudinaryField('django-upload')
     image = CloudinaryField('django-upload')
-    phone_number = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=20, blank=True)
     twitter = models.CharField(max_length=300)
     facebook = models.CharField(max_length=300)
     instagram = models.CharField(max_length=200)
